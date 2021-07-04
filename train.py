Fix invalid action masking

train.py
@@ -2,11 +2,13 @@
 import random
 import gym
 import numpy as np
+import numpy.ma as ma
 from collections import deque
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
+from tqdm import tqdm
 
 EPISODES = 1000
 
@@ -18,12 +20,11 @@ class DQNAgent:
         self.gamma = 0.95    # discount rate
         self.epsilon = 1.0  # exploration rate
         self.epsilon_min = 0.01
-        self.epsilon_decay = 0.995
-        self.learning_rate = 0.001
+        self.epsilon_decay = 0.99
+        self.learning_rate = 0.01
         self.model = self._build_model()
 
     def _build_model(self):
-        # Neural Net for Deep-Q learning Model
         model = Sequential()
         model.add(Dense(24, input_dim=self.state_size, activation='relu'))
         model.add(Dense(24, activation='relu'))
@@ -35,15 +36,22 @@ class DQNAgent:
     def memorize(self, state, action, reward, next_state, done):
         self.memory.append((state, action, reward, next_state, done))
 
-    def act(self, state):
+    def act(self, state, mask):
         if np.random.rand() <= self.epsilon:
-            return random.randrange(self.action_size)
-        act_values = self.model.predict(state)
-        return np.argmax(act_values[0])  # returns action
+            legal = [i for i,f in enumerate(mask) if f]
+            best = random.choice(legal)
+            # print("Explore {}".format(best))
+        else:
+            act_values = self.model.predict(state)
+            act_values = act_values[0]
+            masked = ma.array(act_values, mask=~mask)
+            best = ma.argmax(masked)
+        return best
 
     def replay(self, batch_size):
         minibatch = random.sample(self.memory, batch_size)
-        for state, action, reward, next_state, done in minibatch:
+
+        for state, action, reward, next_state, done in tqdm(minibatch):
             target = reward
             if not done:
                 target = (reward + self.gamma *
@@ -61,39 +69,11 @@ class DQNAgent:
         self.model.save_weights(name)
 
 
-def cartpole():
-        env = gym.make('CartPole-v1')
-        # state_size = env.observation_space.shape[0]
-        # action_size = env.action_space.n
-        agent = DQNAgent(state_size, action_size)
-        # agent.load("./save/cartpole-dqn.h5")
-        done = False
-        batch_size = 32
-
-        for e in range(EPISODES):
-            state = env.reset()
-            state = np.reshape(state, [1, state_size])
-            for time in range(500):
-                # env.render()
-                action = agent.act(state)
-                next_state, reward, done, _ = env.step(action)
-                # reward = reward if not done else -10
-                next_state = np.reshape(next_state, [1, state_size])
-                agent.memorize(state, action, reward, next_state, done)
-                state = next_state
-                if done:
-                    print("episode: {}/{}, reward: {}, e: {:.2}"
-                        .format(e, EPISODES, reward, agent.epsilon))
-                    break
-                if len(agent.memory) > batch_size:
-                    agent.replay(batch_size)
-            # if e % 10 == 0:
-            #     agent.save("./save/cartpole-dqn.h5")
-
 def solid():
     from environment import Environment
     # from environment import Environment
     filename = "../SolidTypes/test/regression/good/mint_MI.sol"
+    # filename = "../SolidTypes/test/regression/good/standard_erc20.sol"
     env = Environment(filename)
     state_size = env.state_size
     action_size = env.action_size
@@ -107,7 +87,7 @@ def solid():
         state = np.reshape(state, [1, state_size])
         for time in range(500):
             # env.render()
-            action = agent.act(state)
+            action = agent.act(state, mask=env.get_mask())
             next_state, reward, done, _ = env.step(action)
             # reward = reward if not done else -10
             next_state = np.reshape(next_state, [1, state_size])