27be797147f2a0b61de4f251b777a8da6ac5df90
Junrui Liu <junrui@ucsb.edu>
2021-07-04 15:11:25 -0400

    Ignore unsupported storage variable types
    Only address, uint256, and nested maps are supported at the moment

--- parse_type.py
+++ parse_type.py
@@ -1,4 +1,4 @@
-from lark import Lark
+import lark
 import typ as T
 
 type_grammar = """
@@ -12,21 +12,26 @@ type_grammar = """
     %ignore WS
 """
 
-parser = Lark(type_grammar)
+parser = lark.Lark(type_grammar)
 
 def parse(type_str):
-    parse_tree = parser.parse(type_str)
     def f(t):
         if t.data == "int":
             return T.Int()
         elif t.data == "mapping":
             return T.Map(f(t.children[1]))
-    return f(parse_tree.children[0])
+    try:
+        parse_tree = parser.parse(type_str)
+        res = f(parse_tree.children[0])
+    except lark.exceptions.UnexpectedCharacters:
+        res = None
+    return res
 
 
 def test(): 
     print(parse("mapping(address => mapping(address => uint256))"))
     print(parse("mapping(uint256 => uint256)"))
+    print(parse("uint8"))
 
 if __name__ == '__main__':
     test()
